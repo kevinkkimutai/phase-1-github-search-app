0bca50675140b554133d802f52b0b6174c7db40c
kevinkkimutai <kiruikevinkim@gmail.com>
2022-12-08 19:26:54 +0300

    updated

--- js/index.js
+++ js/index.js
@@ -1,52 +1,80 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const list = document.querySelector('#user-list')
+  const inputForm = document.querySelector('#github-form')
+  const repoList = document.getElementById("repos-list")
+  fetch('https://api.github.com/users')
+  .then(res => res.json())
+  .then(data => data.forEach(element => {
+    createDom(element, list)
+  }))
 
-const init = () => {
-    const inputForm = document.querySelector('form')
- 
-  
-    inputForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      const input = document.querySelector('input#search');
-  
-     fetch(`https://api.github.com/search/users?q=${input.value}`)
-      .then(response => response.json())
-      .then(data => {
-        (data.items).forEach(user => {
-        const list = document.querySelector('#user-list');
-      list.innerHTML = `
-      <div class="column left">
-      <h2>${user.login}</h2>
-      <img src="${user.avatar_url}"></img
-      <div class="links">
-      <br>
-      <a href="${user.html_url}">${user.html_url}</a>
-      </div>
-      <button class="btn">repos</button
-      </div>
-      `
-
-      });
-
-      
-    });
-    const li = document.querySelector('#user-list')
-    li.addEventListener('click', (event) => {
-      event.preventDefault();
-  
-     fetch(`https://api.github.com/users/${repo.login}/repos`)
-      .then(response => response.json())
-      .then(data => {
-        (data.items).forEach(repo => {
-        const li = document.querySelector('#repos-list');
-      li.innerHTML = `
-      <div class="column left">
-      <h2>${repo.login}</h2>
-      </div>
-      `
-      });
-    });
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = document.querySelector('#search');
+    list.innerHTML = ''
+    handleSearch(input)
   })
+
+  function handleSearch(input) {
+    fetch(`https://api.github.com/search/users?q=${input.value}`)
+    .then(res => res.json())
+    .then(data => {
+      (data.items).forEach(item => {
+        createDom(item, list)
+      })
+    })
+  }
+
+
+
+
+function handleRepos(user) {
+  fetch(`https://api.github.com/users/${user}/repos`)
+  .then(response => response.json())
+  .then(data => {
+     
+      let heading = document.createElement("h3")
+      heading.innerText = `${user} Repos:`
+      repoList.append(heading)
+      data.forEach(item => {
+          const repoName = item["name"]
+          let repoItem = document.createElement("li")
+          repoItem.innerHTML = `
+          <div class="links">
+            <a href="https://github.com/kevinkkimutai/${repoName}">${repoName}</a>
+            </div>
+          `
+          repoList.append(repoItem)
+      })
   })
-  
+}
+
+function createDom(user, li){
+  const item = document.createElement("li")
+  const link = document.createElement("li")
+  item.innerHTML = `
+    <div class="row">
+    <div class="column right">
+    <h2>${user.login}</h2>
+    </div>
+    <div class="column left">
+    <img src="${user.avatar_url}"></img
+    </div>
+    </div>
+    `
+    link.innerHTML = `
+    <div class="links">
+            <a href="https://api.github.com/users/${user.login}">${user.login}</a>
+            </div>
+    `
+  li.appendChild(item)
+  li.appendChild(link)
+  item.addEventListener("click", function() {
+    const user = this.innerText;
+    repoList.innerHTML = ''
+    handleRepos(user)
+})
+
 
 }
-  document.addEventListener('DOMContentLoaded', init);
+})
